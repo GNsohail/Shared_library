8cb2b869bd18e473df966f210cf57ea48471069d
GNsohail <99994215+GNsohail@users.noreply.github.com>
2022-02-23 18:43:39 -0500

    Update checkOutstp.groovy

--- vars/checkOutstp.groovy
+++ vars/checkOutstp.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String gitrep) {
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: "${gitrep}"]]])
+def call(String gitrep, String branchname) {
+    checkout([$class: 'GitSCM', branches: [[name: "${branchname}"]], extensions: [], userRemoteConfigs: [[url: "${gitrep}"]]])
 }
